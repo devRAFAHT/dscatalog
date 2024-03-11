e8667b6dd22acd8ff36dad35cd3968c47d542593
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-03-11 11:31:29 -0300

    Product entity

--- backend/src/main/java/com/andradscorporation/dscatalog/entities/Category.java
+++ backend/src/main/java/com/andradscorporation/dscatalog/entities/Category.java
@@ -3,6 +3,7 @@ package com.andradscorporation.dscatalog.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -14,6 +15,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
 
     public Category(){
     }
