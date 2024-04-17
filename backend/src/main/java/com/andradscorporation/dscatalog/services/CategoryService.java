b98f12235e518375c54389a160b7410d62f31668
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-04-17 08:07:39 -0300

    Refactor pageable

--- backend/src/main/java/com/andradscorporation/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/andradscorporation/dscatalog/services/CategoryService.java
@@ -10,15 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
-import javax.swing.text.html.parser.Entity;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -27,8 +23,8 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
-        Page<Category> list = repository.findAll(pageRequest);
+    public Page<CategoryDTO> findAllPaged(Pageable pageable){
+        Page<Category> list = repository.findAll(pageable);
         return list.map(x -> new CategoryDTO(x));
     }
 
